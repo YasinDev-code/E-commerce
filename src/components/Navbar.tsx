f1f62f7deffdec78333049de96207b674f472b88
YasinDev <yasinnasiry18@gmail.com>
2025-09-30 19:18:47 +0330

    install lucide-react icons , add Footer & Navbar & SearchBar component , compelet Navbar and SearchBar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchBar from "./SearchBar";
+import { Bell, Home, ShoppingCart } from "lucide-react";
+
+export default function Navbar() {
+    return (
+        <nav className="flex items-center justify-between border-b border-gray-200 pb-4">
+            {/* Left section */}
+            <Link href='/' className="flex items-center">
+                <Image
+                    src="/logo.png"
+                    alt="TrendLama"
+                    width={36}
+                    height={36}
+                    className="w-6 h-6 md:w-9 md:h-9"
+                />
+                <p className='text-md font-medium tracking-wider'>TRENDLAMA.</p>
+            </Link>
+            {/* Right section */}
+            <div className="flex items-center space-x-6">
+                <SearchBar />
+                <Link href="/">
+                    <Home className="w-4 h-4 text-gray-600" />
+                </Link>
+                <Bell className="w-4 h-4 text-gray-600" />
+                <ShoppingCart className="w-4 h-4 text-gray-600" />
+                <Link href="/login">
+                    Sign in
+                </Link>
+            </div>
+        </nav>
+    )
+}
