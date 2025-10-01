bac6b06b74584e918b8fe50d7dca1a19d7219306
YasinDev <yasinnasiry18@gmail.com>
2025-10-01 11:14:59 +0330

    completed Categories component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <div className=''>
       {/* Slider */}
-      //TODO: add slider from swiper
+      TODO:add slider from swiper
       <div className="relative aspect-[3/1] mb-12">
         <Image src="/featured.png" alt="featured product" fill />
       </div>
